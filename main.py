31e515755c53f0b8c9d463956f94fa2f6023552b
Shuyuan <kukutou_mikiya@outlook.com>
2023-02-24 11:21:33 -0800

    fix: some bugs in display conditions

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 import flask
 import requests
 
+api_key = "94UcyU0cGrWAaWAD6zABpFsfJKNi6znX"
+
 app = Flask(__name__,static_url_path='/static')
 app.debug = True
 
@@ -17,7 +19,7 @@ def events():
     radius = int(request.args.get("distance"))
     geoPoint = request.args.get("geoPoint")
     category = request.args.get("category")
-    api_key = "94UcyU0cGrWAaWAD6zABpFsfJKNi6znX"
+    
 
     # Map category to segmentId
     category_to_segmentId = {
@@ -32,6 +34,32 @@ def events():
 
     # Send a GET request to the Ticketmaster Event Search API
     url = "https://app.ticketmaster.com/discovery/v2/events.json?keyword={}&radius={}&unit=miles&geoPoint={}&segmentId={}&apikey={}".format(keyword, radius, geoPoint, segmentId, api_key)
+    response = requests.get(url)
+
+    # Return response JSON
+    return jsonify(response.json())
+
+@app.route('/event_detail')
+def event_detail():
+    # Get event ID and API key from JS
+    event_id = request.args.get('eventId')
+
+    url = f'https://app.ticketmaster.com/discovery/v2/events/{event_id}.json?apikey={api_key}'
+
+    # Send a GET request to the Ticketmaster Event Detail API
+    response = requests.get(url)
+
+    # Return response JSON
+    return jsonify(response.json())
+
+@app.route('/venue_detail')
+def venue_detail():
+    # Get venue name and API key from JS
+    venue_name = request.args.get('venueName')
+
+    url = f'https://app.ticketmaster.com/discovery/v2/venues.json?keyword={venue_name}&apikey={api_key}'
+
+    # Send a GET request to the Ticketmaster Venue Search API
     response = requests.get(url)
 
     # Return response JSON
--- static/app.js
+++ static/app.js
@@ -5,6 +5,7 @@ import { GapiKey, apiKey } from './apikey.js';
 let eventsData; // array of events data returned by Ticketmaster Event Search API
 let sortedColumn = -1; // index of column to sort table by
 let sortOrder = 1; // sort order, 1 for ascending and -1 for descending
+const serverUrl = 'https://ticketview-378721.wl.r.appspot.com';
 
 export async function fetchLocation() {
   const response = await fetch("https://ipinfo.io/?token=508efef6f3e453");
@@ -37,7 +38,9 @@ export const getLatLngFromAddress = (address) => {
           longitude: location.lng
         };
       } else {
-        throw new Error('Failed to retrieve location from address');
+        document.getElementById("noResults").style.display = 'block';
+        document.getElementById("eventTable").style.display = 'none';
+        return false;
       }
     });
 };
@@ -47,9 +50,9 @@ export async function search(e) {
   e.preventDefault();
   var form = e.target.form;
   if (form.reportValidity()==false){
-    return
+    return;
   }
-  console.log('search is running')
+
   const keyword = document.getElementById("keyword").value;
   let distance = document.getElementById("distance").value;
   let category = document.getElementById("category").value;
@@ -72,7 +75,7 @@ export async function search(e) {
   // Make the GET request
   
   let geoPoint = Geohash.encode(location.latitude,location.longitude,9);  
-  const url = `http://127.0.0.1:5000/events?keyword=${keyword}&distance=${distance}&category=${category}&geoPoint=${geoPoint}`;
+  const url = `${serverUrl}/events?keyword=${keyword}&distance=${distance}&category=${category}&geoPoint=${geoPoint}`;
   const response = await fetch(url);
   eventsData = await response.json();
   if (eventsData._embedded){
@@ -82,6 +85,7 @@ export async function search(e) {
   }
   else {
       document.getElementById("noResults").style.display = 'block';
+      document.getElementById("eventTable").style.display = 'none';
   }
   return false;
   // Display the results in the result area
@@ -95,14 +99,18 @@ export function clearForm() {
   document.getElementById("category").value = "default";
   document.getElementById("location").value = "";
   document.getElementById("auto-detect").checked = false;
+
   const tableBody = document.querySelector('#eventTable tbody');
     while (tableBody.firstChild) {
     tableBody.removeChild(tableBody.firstChild);
   }
   var tableHead = document.getElementById("tableHead");
   tableHead.hidden = true;
+
+  document.getElementById("noResults").style.display = 'none';
   document.querySelector('#eventCard').style.display = 'none';
-  document.querySelector('#venueDetails').style.display = 'none';
+  document.getElementById('venueToggle').style.display = 'none';
+  document.getElementById('venueDetailsCard').style.display = 'none';
   document.getElementById("location-container").style.display = 'block';
   document.getElementById("location").setAttribute("required", "");
 }
@@ -123,7 +131,8 @@ const displayEvents = () => {
     document.querySelector('#noResults').style.display = 'none';
     var tableHead = document.getElementById("tableHead");
     tableHead.hidden = false;
-  
+    document.getElementById("eventTable").style.display = 'block';
+
     eventsData.forEach(event => {
       const row = document.createElement('tr');
 
@@ -240,8 +249,10 @@ const displayEventCard = (eventId,venueName) => {
   let eventData = false;
   let venueData = false;
 
+  
+
   // make API call to Ticketmaster Event Details API
-  const eventUrl = `https://app.ticketmaster.com/discovery/v2/events/${eventId}.json?apikey=${apiKey}`;
+  const eventUrl = `${serverUrl}/event_detail?eventId=${eventId}`;
   fetch(eventUrl)
     .then(response => {
       if (response.ok) {
@@ -257,8 +268,9 @@ const displayEventCard = (eventId,venueName) => {
     })
     .catch(error => console.error(error));
 
+
   // make API call to Ticketmaster Venue Search API
-  const venueUrl = `https://app.ticketmaster.com/discovery/v2/venues.json?keyword=${encodeURIComponent(venueName)}&apikey=${apiKey}`;
+  const venueUrl = `${serverUrl}/venue_detail?venueName=${encodeURIComponent(venueName)}`;
 
   fetch(venueUrl)
     .then(response => {
@@ -278,14 +290,28 @@ const displayEventCard = (eventId,venueName) => {
 
   // display detailed event card once all API data is returned
   const displayCardInfo = () => {
+    console.log('eventData:');
+    console.log(eventData);
+
     // populate event card with API data
+    const cardEventName = document.querySelector('#cardEventName');
+    const cardDate = document.querySelector('#cardDate');
+    const cardVenue = document.querySelector('#cardVenue');
+    const cardArtist = document.querySelector('#cardArtist');
+    const cardGenres = document.querySelector('#cardGenres');
+    const cardPriceRanges = document.querySelector('#cardPriceRanges');
+    const cardTicketStatus = document.querySelector('#cardTicketStatus');
+    const cardBuyTickets = document.querySelector('#cardBuyTickets');
+    const cardSeatMap = document.querySelector('#cardSeatMap');
+
+
     if (eventData.name) {
       cardEventName.innerHTML = `${eventData.name}`;
     } else {
       cardEventName.style.display = 'none';
     }
 
-    const cardDate = document.querySelector('#cardDate');
+    
     cardDate.style.display = 'block';
     if (eventData.dates.start.localDate && eventData.dates.start.localTime) {
       cardDate.innerHTML = `<strong class="lime-text-size-14">Date</strong><br>${eventData.dates.start.localDate} ${eventData.dates.start.localTime}`;
@@ -293,7 +319,7 @@ const displayEventCard = (eventId,venueName) => {
       cardDate.style.display = 'none';
     }
 
-    const cardArtist = document.querySelector('#cardArtist');
+    
     cardArtist.style.display = 'block';
     if (eventData._embedded.attractions && eventData._embedded.attractions.length > 0) {
       cardArtist.innerHTML = `<strong class="lime-text-size-14">Artist/Team</strong><br>${eventData._embedded.attractions.map(attraction => `<a href="${attraction.url}" style="text-decoration:none">${attraction.name}</a>`).join(' | ')}`;
@@ -301,7 +327,7 @@ const displayEventCard = (eventId,venueName) => {
       cardArtist.style.display = 'none';
     }
 
-    const cardVenue = document.querySelector('#cardVenue');
+    
     cardVenue.style.display = 'block';
     if (eventData._embedded?.venues && eventData._embedded?.venues.length > 0) {
       cardVenue.innerHTML = `<strong class="lime-text-size-14">Venue</strong><br>${eventData._embedded.venues[0].name}`;
@@ -309,19 +335,19 @@ const displayEventCard = (eventId,venueName) => {
       cardVenue.style.display = 'none';
     }
 
-    const cardGenres = document.querySelector('#cardGenres');
+    
     cardGenres.style.display = 'block';
     if (eventData.classifications && eventData.classifications.length > 0) {
-      cardGenres.innerHTML = `<strong class="lime-text-size-14">Genres</strong><br>${eventData.classifications.map(classification => {
-        const names = [classification.subGenre?.name, classification.genre?.name, classification.segment?.name, classification.subType?.name, classification.type?.name];
-        const filteredNames = names.filter(name => name !== "Undefined");
-        return filteredNames.join(' | ');
-      }).join(' | ')}`;
+      const classification = eventData.classifications[0]; // use only the first classification element
+      const names = [classification.subGenre?.name, classification.genre?.name, classification.segment?.name, classification.subType?.name, classification.type?.name];
+      const filteredNames = names.filter(name => (name && name != "Undefined"));
+      console.log(names)
+      console.log(filteredNames)
+      cardGenres.innerHTML = `<strong class="lime-text-size-14">Genres</strong><br>${filteredNames.join(' | ')}`;
     } else {
       cardGenres.style.display = 'none';
     }
-
-    const cardPriceRanges = document.querySelector('#cardPriceRanges');
+    
     cardPriceRanges.style.display = 'block';
     if (eventData.priceRanges && eventData.priceRanges.length > 0) {
       cardPriceRanges.innerHTML = `<strong class="lime-text-size-14">Price Ranges</strong><br>${eventData.priceRanges.map(price => `${price.min} - ${price.max} USD`).join(', ')}`;
@@ -329,7 +355,7 @@ const displayEventCard = (eventId,venueName) => {
       cardPriceRanges.style.display = 'none';
     }
 
-    const cardTicketStatus = document.querySelector('#cardTicketStatus');
+    
     cardTicketStatus.style.display = 'block';
     if (eventData.dates.status && eventData.dates.status.code) {
       const statusCode = eventData.dates.status.code.toLowerCase();
@@ -343,9 +369,9 @@ const displayEventCard = (eventId,venueName) => {
           statusMessage = 'Off Sale';
           statusClass = 'off-sale';
           break;
-        case 'canceled':
-          statusMessage = 'Canceled';
-          statusClass = 'canceled';
+        case 'cancelled':
+          statusMessage = 'Cancelled';
+          statusClass = 'cancelled';
           break;
         case 'postponed':
           statusMessage = 'Postponed';
@@ -357,14 +383,13 @@ const displayEventCard = (eventId,venueName) => {
           break;
         default:
           cardTicketStatus.style.display = 'none';
-          return;
       }
       cardTicketStatus.innerHTML = `<strong class="lime-text-size-14">Ticket status</strong><br><span class="${statusClass} badge">${statusMessage}</span>`;
     } else {
         cardTicketStatus.style.display = 'none';
     }
 
-    const cardBuyTickets = document.querySelector('#cardBuyTickets');
+    
     cardBuyTickets.style.display = 'block';
     if (eventData.url) {
       const ticketmasterLink = document.createElement('a');
@@ -377,13 +402,14 @@ const displayEventCard = (eventId,venueName) => {
       cardBuyTickets.style.display = 'none';
     }
 
-    const cardSeatMap = document.querySelector('#cardSeatMap');
+    
     cardSeatMap.style.display = 'block';
     if (eventData.seatmap && eventData.seatmap.staticUrl) {
+
       cardSeatMap.src = eventData.seatmap.staticUrl;
-      cardSeatMap.alt = "Seat Map Not Available";
     } else {
       cardSeatMap.style.display = 'none';
+
     }
         // show event card and scroll to its location
     document.querySelector('#eventCard').style.display = 'block';
@@ -394,8 +420,7 @@ const displayEventCard = (eventId,venueName) => {
       document.getElementById('venueToggle').style.display = 'block';
       document.getElementById('venueDetailsCard').style.display = 'none';
     } else {
-      document.querySelector('venueDetails').style.display = 'none';
-      console.log(venueData);
+      document.getElementById('venueDetails').style.display = 'none';
     }  
   }
 };
@@ -403,9 +428,8 @@ const displayEventCard = (eventId,venueName) => {
 
 
 const setVenueCard = (venueData) => {
-  console.log(venueData._embedded.venues[0].images)
   // declare variables for venue data
-  let venueName, venueAddress, venueCity, venueState, venuePostalCode, venueLocation, venueImage;
+  let venueName, venueAddress, venueCity, venueState, venuePostalCode, venueLocation, venueImage, venueUrl;
 
   // check if venue data is available
   if (venueData && venueData._embedded && venueData._embedded.venues && venueData._embedded.venues.length > 0) {
@@ -415,10 +439,11 @@ const setVenueCard = (venueData) => {
     venueName = venue.name || 'N/A';
     venueAddress = venue.address?.line1 || 'N/A';
     venueCity = venue.city?.name || 'N/A';
-    venueState = venue.state?.name || 'N/A';
+    venueState = venue.state?.stateCode || 'N/A';
     venuePostalCode = venue.postalCode || 'N/A';
-    venueLocation = `${venueAddress}, ${venueCity}, ${venueState}, ${venuePostalCode}`;
+    venueLocation = `${venueName},${venueAddress},${venueCity},${venueState},${venuePostalCode}`;
     venueImage = venue.images && venue.images.length > 0 ? venue.images[0].url : '';
+    venueUrl = venue.url;
   }
 
   // get the Venue Details card HTML elements
@@ -433,28 +458,36 @@ const setVenueCard = (venueData) => {
   venueDetailsTitle.innerHTML = `${venueName}`;
   if(venueImage){
     venueImageElement.src = venueImage;
+    venueImageElement.width = 240;
+    venueImageElement.height = 135;
   }
   else {
     venueImageElement.style.display = 'none';
   }
   venueDetailsLocation.innerHTML = `
-    <p>Address: ${venueAddress}<br>${venueCity}, ${venueState}<br>${venuePostalCode}</p>
+    <p><strong>Address:</strong> ${venueAddress}<br>${venueCity}, ${venueState}<br>${venuePostalCode}</p>
     <p><a href="https://www.google.com/maps/search/?api=1&query=${encodeURI(venueLocation)}" style="text-decoration:none; color: rgb(0, 191, 255);" target="_blank">Open in Google Maps</a></p>
   `;
-  venueDetailsEvents.innerHTML = `
-    <p><a href="https://www.ticketmaster.com/search?q=${encodeURI(venueName)}" style="text-decoration:none; color: rgb(0, 191, 255);" target="_blank">More events at this venue</a></p>
+  if (venueUrl) {
+    venueDetailsEvents.innerHTML = `
+    <p><a href="${venueUrl}" style="text-decoration:none; color: rgb(0, 191, 255);" target="_blank">More events at this venue</a></p>
   `;
+  }
+  
+  console.log('venueLocation:')
+  console.log(venueLocation)
+  console.log('venueUrl:')
+  console.log(venueUrl)
   // hide the Venue Details card initially
   venueDetailsCard.style.display = 'none';
-
+  document.querySelector('#venueToggle').style.display = 'block';
   const venueToggle = document.querySelector('#venueToggle')
   // add event listener to show/hide Venue Details card on click
   venueToggle.addEventListener('click', () => {
     if (venueDetailsCard.style.display === 'none') {
       venueDetailsCard.style.display = 'block';
       venueToggle.style.display = 'none';
-    } else {
-      venueDetailsWrapper.style.display = 'none';
+      venueDetailsCard.scrollIntoView();
     }
   });
 };
